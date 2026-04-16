32a9c76ff3c987c6c088c4715094f4ec19612d2c
idontwanttobeanurseanymore <martaariasolmo@hotmail.com>
2026-04-16 20:47:56 +0200

    css needs a good review

--- src/js/main.js
+++ src/js/main.js
@@ -19,17 +19,36 @@ selected.addEventListener("click", () => {
 });
 
 //Seleccionar opción
-options.forEach(option => {
+for(const option of options){
   option.addEventListener("click", () => {
-    selected.innerHTML = option.innerHTML;
-    hiddenInput.value = option.dataset.value; //guardala en una variable
+    const value = option.dataset.value;
+    hiddenInput.value = value; //guardala en una variable
     optionsBox.style.display = "none";
-  });
-});
+    renderDesign(value);
+    //console.log(option)
+})}
 
 // Cerrar si haces click fuera
-document.addEventListener("click", (e) => {
-  if (!e.target.closest(".js_customSelect")) {
+document.addEventListener("click", (ev) => {
+  if (!ev.target.closest(".js_customSelect")) {
     optionsBox.style.display = "none";
     }
 })
+//console.log(options)
+
+//render
+function renderDesign(value){
+  selected.innerHTML = '';
+  const option = document.querySelector(`[data-value="${value}"]`);
+  const theme = option.dataset.value;
+  if(!option) return;
+  const prueba = document.querySelector('.prueba');
+  prueba.className='prueba';
+  prueba.classList.add(theme);
+  prueba.innerHTML = `
+    <h3>Holi</h3>
+    <p>holi holi</p>
+    `;
+  console.log(value) 
+  console.log(option)// está guardando el valor, falta la preview
+}
