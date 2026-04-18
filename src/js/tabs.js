d20c6b16b2bf489d2cd821b00481540186d1a17f
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-04-18 11:53:51 +0200

    created tabs javascript file. added js variables. created utilities folder and visibility class.

---
+++ src/js/tabs.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//SECCIÓN DE QUERY-SELECTOR
+const designSection = document.querySelector(".js_designSection");
+const fillSection = document.querySelector(".js_fillSection");
+const shareSection = document.querySelector(".js_shareSection");
+const tabDesignBtn = document.querySelector(".js_tabDesignBtn");
+const tabFillBtn = document.querySelector(".js_tabFillBtn");
+const tabShareBtn = document.querySelector(".js_tabShareBtn");
+
+//SECCIÓN DE DATOS
+
+//SECCIÓN DE FUNCIONES
+
+//SECCIÓN DE FUNCIONES DE EVENTOS
+
+//SECCIÓN DE EVENTOS
+
+//SECCIÓN DE ACCIONES AL CARGAR LA PÁGINA
